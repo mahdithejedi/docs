cf2a0c0c0638916f641540ec2ee7a454c1ad37a4
Seyedmm021 <seyedmm021@gmail.com>
2020-05-25 21:30:41 +0430

    POSTGRES datetime update

--- POSTGRES/DATETIME/eg2.sql
+++ POSTGRES/DATETIME/eg2.sql
@@ -1,4 +1,10 @@
-SELECT SUM(amount)as expenses, EXTRACT(month from payment_date) AS monthes
+SELECT SUM(amount) as t_exp, EXTRACT(MONTH from payment_date) AS pay_months
 FROM payment
-GROUP BY monthes
-ORDER BY expenses DESC;
+GROUP BY pay_months -- Here we group the all the rows in payment by month and the calculate the sum
+ORDER BY t_exp DESC
+
+/*
+POINT: you think that the first think is GROUP BY
+first the pyment_date will group by monthes then after that 
+the SUM of pay_month or the amount of all the same monthes will be execute
+*/
