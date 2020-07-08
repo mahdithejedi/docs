272988328ce8998d8ec08c439f0328b4f30b2a88
seyedmm021 <seyedmm021@gmail.com>
2020-07-08 09:04:13 +0430

    [UPDATE] python sysloghandler

--- PYTHON/PythonLang/Logging/eg/SuccessFullExample/SyslogHandler.py
+++ PYTHON/PythonLang/Logging/eg/SuccessFullExample/SyslogHandler.py
@@ -8,14 +8,14 @@ APP_NAME = 'UssdApp'
 #set handler for calling UNIX syslog to handle log throught UDP:514
 #POINT: so our system should have a open port in 514
 Handler=logging.handlers.SysLogHandler(address='/dev/log')
-formatter  = logging.Formatter(FMT, DATEFMT)
+formatter  = logging.Formatter('%(message)s', DATEFMT)
 Handler.setFormatter(formatter)
 Handler.ident = APP_NAME
 
-logger = logging.getLogger()
+logger = logging.getLogger(APP_NAME)
 logger.addHandler(Handler)
 logger.setLevel(logging.DEBUG)
 
-logger.info('This  is  a simple info In logger Test')
+logger.info('\n\n\n\n\n\n\n\n\nNEW \n\n\n\n\n\n\n\n\n\n')
 
 logger.critical('Oh SHIT ERROR! CRITICAL ONE IS HAPPENED')
