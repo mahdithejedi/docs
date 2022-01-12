4452906ed0c3ec80e9395b5281f49e4c8ee36974
seyedmm021 <seyedmm021@gmail.com>
2022-01-12 15:45:16 +0330

    [UPDATE] GO [UPDATE] bst tree now can insert data without ordering

--- ALGORITHM/TREE/BST/bst.go
+++ ALGORITHM/TREE/BST/bst.go
@@ -1,28 +1,31 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	value int
 	left  *Node
 	right *Node
 }
 
-func FindBestPosition(n *Node, new_value int) Node {
-	if new_value == n.value {
-		return *n
+func (n *Node) insertInTree(new_value int) *Node {
+	if n == nil {
+		n = &Node{value: new_value}
 	} else if n.value > new_value {
-		return FindBestPosition(n.left, new_value)
+		n.left = n.left.insertInTree(new_value)
 	} else {
-		return FindBestPosition(n.right, new_value)
+		n.right = n.right.insertInTree(new_value)
 	}
+	return n
 }
 
 func (n *Node) Insert(new_value int) {
+	// initiation!
 	if n.value == 0 {
 		n.value = new_value
 		return
 	}
-	// postion_to_insert := FindBestPosition(n, new_value)
-	// postion_to_insert.value = new_value
+	n.insertInTree(new_value)
 
 }
 
@@ -31,4 +34,6 @@ func main() {
 	n.Insert(12)
 	n.Insert(10)
 	n.Insert(14)
+	n.Insert(11)
+	fmt.Println(n.left.right.value)
 }
