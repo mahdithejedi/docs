95064f403a12c4daa541379092739ff92a4915a9
seyedmm021 <seyedmm021@gmail.com>
2022-03-12 10:03:16 +0330

    [UPDATE] GO [Bugfix] URLshortner can't import handler

--- GO/Projects/URLshortner/core/handler.go
+++ GO/Projects/URLshortner/core/handler.go
@@ -14,7 +14,7 @@ func remote_dsn(name string, password string, host net.IP, port int) string {
 	return fmt.Sprintf("%s:%s@tcp(%s,%d)", name, password, host, port)
 }
 
-func initiate_DB(name string, password string, host string, port int) *gorm.DB {
+func Initiate_DB(name string, password string, host string, port int) *gorm.DB {
 	fmt.Print("Go to initiate")
 	dsn := remote_dsn(name, password, net.ParseIP(host), port)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
