26228615718fb26738fb76e162787cae7f0e016b
Mahdi Moosavi <seyedmm021@gmail.com>
2022-08-29 19:35:50 +0430

    [UPDATE] Postgresql [UPDATE] query history

--- POSTGRES/_query_history.sql
+++ POSTGRES/_query_history.sql
@@ -9,7 +9,16 @@ select order_id from orders_order inner join markets_market mm on mm.id = orders
 WITH price_sum_ali AS (
 	    select market_id, created_at, updated_at, order_id, status ,price ,sum(price) over (order by created_at, updated_at) as price_sum from orders_order where market_id = 4 and direction = 'SELL' and status = 'processing'
 ) select order_id from price_sum_ali where price_sum < 101.77200000000002;
+-- Group by 1 hours interval
+select count(*),
+       date_trunc('hour', o.created_at)  AS ten_min_timestamp
+       from orders_order o inner join markets_market mm on mm.id = o.market_id where
+    cancel_by = 'limit_bot' group by ten_min_timestamp order by ten_min_timestamp desc
 
-
-
-
+-- Groupy by 10 minutes interval
+select count(*),
+       date_trunc('hour', o.created_at) +
+       (((date_part('minute', o.created_at)::integer / 10::integer) * 10::integer)
+	           || ' minutes')::interval AS ten_min_timestamp
+	       from orders_order o inner join markets_market mm on mm.id = o.market_id where
+	    cancel_by = 'limit_bot' group by ten_min_timestamp
