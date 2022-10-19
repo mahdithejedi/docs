2204ecc2def574cd07a3d9e996bbdf2d4c494746
Mahdi Moosavi <seyedmm021@gmail.com>
2022-10-19 17:56:57 +0330

    [UPDATE] SmartContract [ADD] More comments

--- BLOCKCHAIN/Ethereum/SmartContract/MessageExample/contracts/Index.sol
+++ BLOCKCHAIN/Ethereum/SmartContract/MessageExample/contracts/Index.sol
@@ -1,5 +1,13 @@
 pragma solidity 0.8.17;
 
+// #__DOCUMENTATION__ #__SOLIDITY__
+// This code is publish into etherium blockchain
+// so everybody will see it so we shouldn't put secret bussiness logic in here
+//-----
+// gasPrice -> amount of wei we are pay for a gas
+// gasLimit -> MAX amount of gas we are willing to pay
+// !!! gasLimit > Amount of gas our transaction need (ramainig gas will return back)
+
 contract Index {
     //  because it's public
     //it's available to all users in the world
@@ -12,12 +20,16 @@ contract Index {
     }
 
     function setMessage(string memory newMessage) public {// 'public' or 'private'
+        //! Every data that modified in etherium blockchain should be in a form of TRANSACTION(
+        //! this transaction should be mine and proved + cost money + take time to execute + return transaction hash)
+        //
         message = newMessage;
     }
 
     function getMessage() public view returns (string memory){// 'view' = 'constant' means this function returns data and NOT modify contract's data
         //        'pure' -> Function will not modify or even read the contract's data
         //        'payable' -> When s1 call this function they might send ether along
+        //! This function can not modify data + run instantly + free to do
         return message;
     }
 }
