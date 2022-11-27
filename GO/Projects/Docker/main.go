cd0eee55a9e052f11d084c802938a898e960210e
Mahdi Moosavi <seyedmm021@gmail.com>
2022-11-27 14:02:56 +0330

    [UPDATE] Goland [UPDATE] Docker Simplified

--- GO/Projects/Docker/main.go
+++ GO/Projects/Docker/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -14,6 +17,11 @@ import (
 // what is pid namespace? = 'https://man7.org/linux/man-pages/man7/pid_namespaces.7.html'
 
 // __source__ = 'https://youtu.be/8fi7uSYlOdc'
+// Github source = 'https://github.com/lizrice/containers-from-scratch/blob/master/main.go'
+
+// Namespace => what can you see?
+// Cgroup (Control Group) => what can you do?
+// Location /sys/fs/cgroup => different Cgroup
 
 func checkError(err error) {
 	if err != nil {
@@ -30,10 +38,8 @@ func run(command string, args ...string) {
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
-		//Unshareflags: syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID, /* | syscall.CLONE_NEWUSER | syscall.CLONE_NEWIPC */
 	}
-	//syscall.Sethostname([]byte("parent"))
 	checkError(cmd.Run())
 	fmt.Println("------------------------------------")
 }
@@ -41,10 +47,36 @@ func run(command string, args ...string) {
 func child(command string, args ...string) {
 	fmt.Println("Running", os.Args[2:], "as", os.Getpid())
 	fmt.Println("------------------------------------")
+	setSyscall()
+	setCgroup()
+	runChild(command, args...)
+}
+
+func setSyscall() {
+	// What is chroot?
+	// https://www.geeksforgeeks.org/chroot-command-in-linux-with-examples/
 	syscall.Sethostname([]byte("child"))
 	syscall.Chroot("/")
 	syscall.Chdir("/home/m.moosavi/")
-	//syscall.Mount("proc", "proc", "proc", 0, "")
+
+}
+
+func setCgroup() {
+	// Limit number of processes to 20
+	cgroups := "/sys/fs/cgroup/"
+	pids := filepath.Join(cgroups, "pids")
+	os.Mkdir(filepath.Join(pids, "liz"), 0755)
+	checkError(ioutil.WriteFile(filepath.Join(pids, "liz/pids.max"), []byte("20"), 0700))
+	// Removes the new cgroup in place after the container exits
+	checkError(ioutil.WriteFile(filepath.Join(pids, "liz/notify_on_release"), []byte("1"), 0700))
+	// add current process into this group
+	checkError(ioutil.WriteFile(filepath.Join(pids, "liz/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+	// If you run command on shell
+	//	:() { : | : & } ; :
+	// Then you system won't get fucked because you have limited processes number up to 20
+}
+
+func runChild(command string, args ...string) {
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
