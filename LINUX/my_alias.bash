da1f8a24e2246531bca74cbbea01021b3764958f
Mahdi Moosavi <seyedmm021@gmail.com>
2022-12-31 13:36:02 +0330

    [UPDATE] Linux [ADD] Add My alias

---
+++ LINUX/my_alias.bash
@@ -0,0 +1,14 @@
+alias p='git pull'
+alias f='git fetch'
+alias fa='git fetch --all'
+function push() {
+    while true; do
+      echo "Do you want to push to $(git branch --show-current)??"
+        read -r yn
+        case $yn in
+            [Yy]* ) git push; break;;
+            [Nn]* ) break ;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
