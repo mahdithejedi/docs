19d59dbd5531a7b929224c3f2759369cc04bd9de
Mahdi Moosavi <seyedmm021@gmail.com>
2023-03-18 16:59:54 +0330

    [UPDATE] GO -> PostgresqlNotify  [ADD]  Add POSTGRESQL Starter Command

--- GO/PostgresqlNotifyFollower/cmd/listener.go
+++ GO/PostgresqlNotifyFollower/cmd/listener.go
@@ -21,6 +21,19 @@ const (
 
 var State = STOPPED
 
+func preRun(cmd *cobra.Command, args []string) {
+	if State == RUNNING {
+		fmt.Fprintln(os.Stderr, "Listener is currently running")
+		os.Exit(0)
+	}
+}
+
+func run(cmd *cobra.Command, args []string) {
+	config := listener.LoadConfig("/home/m.moosavi/Desktop/Projects/docs/GO/PostgresqlNotifyFollower/conf.ini")
+	State = RUNNING
+	listener.Run(config, db.Connect(config))
+}
+
 // listenerCmd represents the listener command
 var listenerCmd = &cobra.Command{
 	Use:     "listener",
@@ -28,27 +41,47 @@ var listenerCmd = &cobra.Command{
 	Long:    `Postgresql main listener here you can listen to a postgresql event and do something`,
 	Version: "0.1-alpha",
 	PreRun: func(cmd *cobra.Command, args []string) {
-		if State == RUNNING {
-			fmt.Fprintln(os.Stderr, "Listener is currently running")
-			os.Exit(0)
-		}
+		preRun(cmd, args)
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		config := listener.LoadConfig("/home/m.moosavi/Desktop/Projects/docs/GO/PostgresqlNotifyFollower/conf.ini")
-		listener.Run(config, db.Connect(config))
+		run(cmd, args)
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		PostStop()
 	},
 }
 
+var listenerStartCmd = &cobra.Command{
+	Use:     "start",
+	Aliases: []string{"run", "enable"},
+	Short:   "Postgresql start listener",
+	Long:    `Postgresql main listener here you can listen to a postgresql event and do something`,
+	Version: "0.1-alpha",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		preRun(cmd, args)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		run(cmd, args)
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		PostStop()
+	},
+}
+
+func PostStop() {
+	State = STOPPED
+}
 func init() {
 	rootCmd.AddCommand(listenerCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listenerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listenerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	listenerCmd.AddCommand(listenerStartCmd)
 }
+
+// Here you will define your flags and configuration settings.
+
+// Cobra supports Persistent Flags which will work for this command
+// and all subcommands, e.g.:
+// listenerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+// Cobra supports local flags which will only run when this command
+// is called directly, e.g.:
+// listenerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
